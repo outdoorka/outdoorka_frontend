f44e62f3eb31c7781ee7ddf8b7fa457d5394a4ba
louisa0416 <louisa04162018@gmail.com>
2024-05-15 22:24:17 +0800

    feat: 增加 Header 未登入狀態切版

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,62 @@
 import React from "react";
 import { Box } from "@mui/material";
+import Header from "@/components/layout/header/header";
 import Footer from "@/components/layout/footer/footer";
 
 function Home() {
 	return (
-		<Box component="main">
-			main page
+		<>
+			<Header />
+			<Box component="main">
+				起main page Lorem ipsum dolor sit amet consectetur adipisicing elit.
+				Asperiores facilis inventore quae, sapiente maxime sed deleniti laborum
+				provident porro dignissimos quisquam, repellat, veritatis sunt hic
+				ducimus! Nam sequi pariatur perferendis. Lorem ipsum dolor sit amet
+				consectetur adipisicing elit. Asperiores facilis inventore quae,
+				sapiente maxime sed deleniti laborum provident porro dignissimos
+				quisquam, repellat, veritatis sunt hic ducimus! Nam sequi pariatur
+				perferendis. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+				Asperiores facilis inventore quae, sapiente maxime sed deleniti laborum
+				provident porro dignissimos quisquam, repellat, veritatis sunt hic
+				ducimus! Nam sequi pariatur perferendis. Lorem ipsum dolor sit amet
+				consectetur adipisicing elit. Asperiores facilis inventore quae,
+				sapiente maxime sed deleniti laborum provident porro dignissimos
+				quisquam, repellat, veritatis sunt hic ducimus! Nam sequi pariatur
+				perferendis. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+				Asperiores facilis inventore quae, sapiente maxime sed deleniti laborum
+				provident porro dignissimos quisquam, repellat, veritatis sunt hic
+				ducimus! Nam sequi pariatur perferendis. Lorem ipsum dolor sit amet
+				consectetur adipisicing elit. Asperiores facilis inventore quae,
+				sapiente maxime sed deleniti laborum provident porro dignissimos
+				quisquam, repellat, veritatis sunt hic ducimus! Nam sequi pariatur
+				perferendis. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+				Asperiores facilis inventore quae, sapiente maxime sed deleniti laborum
+				provident porro dignissimos quisquam, repellat, veritatis sunt hic
+				ducimus! Nam sequi pariatur perferendis. Lorem ipsum dolor sit amet
+				consectetur adipisicing elit. Asperiores facilis inventore quae,
+				sapiente maxime sed deleniti laborum provident porro dignissimos
+				quisquam, repellat, veritatis sunt hic ducimus! Nam sequi pariatur
+				perferendis. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+				Asperiores facilis inventore quae, sapiente maxime sed deleniti laborum
+				provident porro dignissimos quisquam, repellat, veritatis sunt hic
+				ducimus! Nam sequi pariatur perferendis. Lorem ipsum dolor sit amet
+				consectetur adipisicing elit. Asperiores facilis inventore quae,
+				sapiente maxime sed deleniti laborum provident porro dignissimos
+				quisquam, repellat, veritatis sunt hic ducimus! Nam sequi pariatur
+				perferendis. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+				Asperiores facilis inventore quae, sapiente maxime sed deleniti laborum
+				provident porro dignissimos quisquam, repellat, veritatis sunt hic
+				ducimus! Nam sequi pariatur perferendis. Lorem ipsum dolor sit amet
+				consectetur adipisicing elit. Asperiores facilis inventore quae,
+				sapiente maxime sed deleniti laborum provident porro dignissimos
+				quisquam, repellat, veritatis sunt hic ducimus! Nam sequi pariatur
+				perferendis. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+				Natus, unde quos mollitia sed non voluptas rerum est veritatis, at
+				consectetur excepturi fugit doloremque sequi accusamus earum cupiditate
+				enim doloribus reprehenderit!
+			</Box>
 			<Footer />
-		</Box>
+		</>
 	);
 }
 
--- components/layout/header/header.tsx
+++ components/layout/header/header.tsx
@@ -1,7 +1,122 @@
 import React from "react";
 
+import {
+	AppBar,
+	Box,
+	Link,
+	Typography,
+	Avatar,
+	IconButton,
+	Badge,
+	Button,
+	Toolbar,
+} from "@mui/material";
+
+const linkTiltes = [
+	{
+		title: "關於我們",
+		link: "#",
+	},
+	{
+		title: "活動",
+		link: "#",
+	},
+	{
+		title: "優良主揪",
+		link: "#",
+	},
+	{
+		title: "Blog",
+		link: "#",
+	},
+	{
+		title: "短影音",
+		link: "#",
+	},
+];
+
 function Header() {
-	return <div>Header</div>;
+	return (
+		<div>
+			<AppBar position="sticky" color="inherit">
+				<Toolbar sx={{ backgroundColor: "#FDFCFF", padding: 2 }}>
+					{linkTiltes.map((item, index) => (
+						<Link
+							color="inherit"
+							fontSize="inherit"
+							noWrap
+							key={index}
+							// variant="body2"
+							href={item.link}
+							underline="hover"
+							sx={{
+								padding: 1,
+								marginRight: 3,
+								flexShrink: 0,
+								textDecoration: "none",
+
+								// text-decoration:"none";
+							}}
+							// className={classes.toolbarLink}
+						>
+							{item.title}
+						</Link>
+					))}
+					<Typography
+						component="h2"
+						variant="h5"
+						color="inherit"
+						align="center"
+						noWrap
+						flex="1"
+						// className={classes.toolbarTitle}
+					>
+						<Box
+							component="img"
+							// marginRight={0.5}
+							src="images/logoHeader_2.png"
+							alt="揪好咖"
+						></Box>
+					</Typography>
+					{/* 登入後 ------------------------------------------------*/}
+					{/* <Box>
+					<Box
+						borderRadius="5rem"
+						bgcolor="#EFF0F7"
+						display="inline-flex"
+						justifyContent="flex-start"
+						spacing={20}
+						padding={0.5}
+						alignItems="center"
+					>
+						<Avatar sx={{ width: 40, height: 40 }}>M</Avatar>
+						<Box color="#22252A" margin={1}>
+							CIAO! Samew
+						</Box>
+					</Box>
+					<IconButton color="inherit">
+						<Badge badgeContent={4} color="secondary">
+							票卷
+						</Badge>
+					</IconButton>
+					<IconButton color="inherit">
+						<Badge badgeContent={4} color="secondary">
+							愛心
+						</Badge>
+					</IconButton>
+				</Box> */}
+					{/* 登入前 ------------------------------------------------*/}
+					<Button color="inherit" href="/login">
+						登入
+					</Button>
+					|
+					<Button color="inherit" href="/register">
+						註冊
+					</Button>
+				</Toolbar>
+			</AppBar>
+		</div>
+	);
 }
 
 export default Header;
