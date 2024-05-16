465c753efdce035511cd9b1a50d1b7f8357a211c
Andy Lee <ltsungmin1345@gmail.com>
2024-05-16 22:36:33 +0800

    fix: edit import name

--- components/layout/MainLayout/MainLayout.tsx
+++ components/layout/MainLayout/MainLayout.tsx
@@ -1,7 +1,7 @@
 import React, { ReactNode } from "react";
 import { Box, Typography } from "@mui/material";
 import Header from "@/components/layout/Header/Header";
-import Footer from "@/components/layout/Footer/footer";
+import Footer from "@/components/layout/Footer/Footer";
 import { MainLayoutProps } from "@/types/index";
 
 function MainLayout({ children }: MainLayoutProps) {
