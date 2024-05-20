98fcc736d26c0e52dbe9ee162eb008afc8f6f07a
Ahow <auaicj8@gmail.com>
2024-05-20 12:05:58 +0800

    style: 新增字體、fontawesome

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
 import React from "react";
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
 import { Box } from "@mui/material";
+config.autoAddCss = false;
 
 function Home() {
 	return <Box>main page</Box>;
