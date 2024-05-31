43f752cb28c45b898305f716b552e666b7a5fb52
IngridKao <ingrid7613@gmail.com>
2024-05-31 16:53:50 +0800

    feat: 🎉 調整主揪列表到首頁

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 
 import MainLayout from "@/components/layout/MainLayout/MainLayout";
+import HomeOrganizerCarousel from "@/components/ui/carousel/homeOrganizerCarousel";
 import HotActivities from "@/components/layout/home/HotActivities";
 import NewActivities from "@/components/layout/home/NewActivities";
+
 import { Box, Unstable_Grid2 as Grid } from "@mui/material";
 
 import { config } from "@fortawesome/fontawesome-svg-core";
@@ -14,7 +16,7 @@ function Home() {
 		<MainLayout>
 			<Grid container justifyContent="center">
 				<Grid xs={12}>
-					<Box sx={{ height: "80vh", backgroundColor: "#F8F9FF" }}>banner</Box>
+					首頁Banner幻燈片
 				</Grid>
 				<Grid xs={12} sx={{ maxWidth: "1536px" }}>
 					<HotActivities />
@@ -22,13 +24,13 @@ function Home() {
 				<Grid xs={12}>
 					<NewActivities />
 				</Grid>
-				<Grid xs={10}>
+				<Grid xs={10} sx={{ maxWidth: "1536px", mb: "200px" }}>
 					<Box sx={{ height: "80vh", backgroundColor: "#F8F9FF" }}>
 						活動探索
 					</Box>
 				</Grid>
-				<Grid xs={12}>
-					<Box sx={{ height: 400, backgroundColor: "#F8F9FF" }}>優質主揪</Box>
+				<Grid xs={12} sx={{ maxWidth: "1536px", mb: "200px" }}>
+					<HomeOrganizerCarousel />
 				</Grid>
 			</Grid>
 		</MainLayout>
